5291c3b984b3254921b7c44ceb1e4ae3701c276c
Marco Degano <marcobdegano@gmail.com>
2021-02-18 10:13:35 -0500

    Adjusted docker scripts for cron

--- monthly_make_docker.sh
+++ monthly_make_docker.sh
@@ -1,2 +1,2 @@
-docker build -t monthly_community --build-arg MYSQL_WOMS_PROD=$MYSQL_WOMS_PROD --build-arg API_KEY_POSTMARK=$API_KEY_POSTMARK .
-docker run monthly_community python Monthly_Cost_WO.py
+/usr/local/bin/docker build -t monthly_community --build-arg MYSQL_WOMS_PROD=$MYSQL_WOMS_PROD --build-arg API_KEY_POSTMARK=$API_KEY_POSTMARK .
+/usr/local/bin/docker run monthly_community python Monthly_Cost_WO.py
--- weekly_make_docker.sh
+++ weekly_make_docker.sh
@@ -1,3 +1,3 @@
-docker build -t weekly_community --build-arg MYSQL_WOMS_PROD=$MYSQL_WOMS_PROD --build-arg API_KEY_POSTMARK=$API_KEY_POSTMARK .
-docker run weekly_community python Weekly_Facebook_Community_Results.py
-docker run --rm weekly_community python Weekly_GMB_Community_Results.py
+/usr/local/bin/docker build -t weekly_community --build-arg MYSQL_WOMS_PROD=$MYSQL_WOMS_PROD --build-arg API_KEY_POSTMARK=$API_KEY_POSTMARK .
+/usr/local/bin/docker run weekly_community python Weekly_Facebook_Community_Results.py
+/usr/local/bin/docker run --rm weekly_community python Weekly_GMB_Community_Results.py
